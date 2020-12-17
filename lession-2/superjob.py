4816bd5fdbb219effac32b84c4c7c87733cc4da9
kt-tm <katya.melnikowa@mail.ru>
2020-12-18 00:13:01 +0300

    ДЗ 4

--- lession-2/superjob.py
+++ lession-2/superjob.py
@@ -1,88 +1,90 @@
-from bs4 import BeautifulSoup as bs
-import requests
-import re
-from pymongo import MongoClient
-client = MongoClient('127.0.0.1', 27017)
-db = client['vacancies']
-vacancydb = db.vacancies
-
-vacancy_input = 'менеджер по продажам авто'
-    # input('Ввведите название вакансии ')  # 'менеджер по продажам авто'
-# pr_reload_file = input('Введите yes, если необходимо перезаписать файл с результатами ')
-main_link = 'https://www.superjob.ru'
-
-addit_params = f"/vacancy/search/?keywords={vacancy_input}&noGeo=1"
-headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'}
-vacancies = []
-cnt = 0
-def insert_uniq(data):
-    if vacancydb.count_documents({'link': data['link']}) == 0:
-        vacancydb.insert_one({
-            "site": data['site'],
-            "vacancy": data['vacancy'],
-            "link": data['link'],
-            "currency": data['currency'],
-            "salary_min": data['salary_min'],
-            "salary_max": data['salary_max']})
-        return True
-    else:
-        return False
-
-while True:
-    response = requests.get(main_link + addit_params, params={}, headers=headers)
-    soup = bs(response.text, 'html.parser')
-    if response.ok:
-        vacancy_list = soup.findAll('div', {'class': 'jNMYr'})
-        for vacancy in vacancy_list:
-            data = {}
-            cnt += 1
-            vacancy_header = vacancy.find('a', {'class': '_6AfZ9'})
-            data['site'] = 'https://www.superjob.ru'
-            data['vacancy'] = vacancy_header.text
-            data['link'] = 'https://www.superjob.ru' + vacancy_header['href']
-            data['currency'] = None
-            data['salary_min'] = None
-            data['salary_max'] = None
-            vacancy_salary = vacancy.find('span', {'class': '_2Wp8I'})
-            try:
-                salary_text = ''.join(vacancy_salary.text.split()[:-1])
-                data['currency'] = vacancy_salary.text.split()[-1]
-                data['vacancy_salary']=vacancy_salary
-                if data['currency'] == 'договорённости':
-                    data['currency'] = None
-                    data['salary_min'] = None
-                    data['salary_max'] = None
-                elif salary_text.find('-') > -1:
-                    data['salary_min'] = int(re.sub('\D', '', salary_text.split('-')[0]))
-                    data['salary_max'] = int(re.sub('\D', '', salary_text.split('-')[-1]))
-                elif salary_text.find('—') > -1:
-                    data['salary_min'] = int(re.sub('\D', '', salary_text.split('—')[0]))
-                    data['salary_max'] = int(re.sub('\D', '', salary_text.split('—')[-1]))
-                elif salary_text.find('от') > -1:
-                    data['salary_min'] = int(re.sub('\D', '', salary_text))
-                    data['salary_max'] = None
-                elif salary_text.find('до') > -1:
-                    data['salary_min'] = None
-                    data['salary_max'] = int(re.sub('\D', '', salary_text))
-                else:
-                    data['salary_min'] = int(re.sub('\D', '', salary_text))
-                    data['salary_max'] = int(re.sub('\D', '', salary_text))
-                vacancies.append(data)
-                insert_uniq(data)
-            except AttributeError:
-                continue
-        next_link = soup.find('a', {'class': 'f-test-link-Dalshe'})
-        if next_link is None:
-            break
-        addit_params = next_link['href']
-
-# if pr_reload_file == 'yes':
-#     with open("result.csv", "w", encoding="utf-8") as my_file:
-#         my_file.write("")
+# from bs4 import BeautifulSoup as bs
+# import requests
+# import re
+# from pymongo import MongoClient
+# client = MongoClient('127.0.0.1', 27017)
+# db = client['vacancies']
+# vacancydb = db.vacancies
 #
-# with open("result.csv", "a", encoding="utf-8") as my_file:
-#     for v in vacancies:
-#         try:
-#             my_file.write(f"{v['site']};{v['link']};{v['vacancy']};{v['salary_min']};{v['salary_max']};{v['currency']}\n")
-#         except KeyError:
-#             print(v)
+# vacancy_input = 'менеджер по продажам авто'
+#     # input('Ввведите название вакансии ')  # 'менеджер по продажам авто'
+# # pr_reload_file = input('Введите yes, если необходимо перезаписать файл с результатами ')
+# main_link = 'https://www.superjob.ru'
+#
+# addit_params = f"/vacancy/search/?keywords={vacancy_input}&noGeo=1"
+# headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'}
+# vacancies = []
+# cnt = 0
+# def insert_uniq(data):
+#     if vacancydb.count_documents({'link': data['link']}) == 0:
+#         vacancydb.insert_one({
+#             "site": data['site'],
+#             "vacancy": data['vacancy'],
+#             "link": data['link'],
+#             "currency": data['currency'],
+#             "salary_min": data['salary_min'],
+#             "salary_max": data['salary_max']})
+#         return True
+#     else:
+#         return False
+#
+# while True:
+#     response = requests.get(main_link + addit_params, params={}, headers=headers)
+#     soup = bs(response.text, 'html.parser')
+#     if response.ok:
+#         vacancy_list = soup.findAll('div', {'class': 'jNMYr'})
+#         for vacancy in vacancy_list:
+#             data = {}
+#             cnt += 1
+#             vacancy_header = vacancy.find('a', {'class': '_6AfZ9'})
+#             data['site'] = 'https://www.superjob.ru'
+#             data['vacancy'] = vacancy_header.text
+#             data['link'] = 'https://www.superjob.ru' + vacancy_header['href']
+#             data['currency'] = None
+#             data['salary_min'] = None
+#             data['salary_max'] = None
+#             vacancy_salary = vacancy.find('span', {'class': '_2Wp8I'})
+#             try:
+#                 salary_text = ''.join(vacancy_salary.text.split()[:-1])
+#                 data['currency'] = vacancy_salary.text.split()[-1]
+#                 data['vacancy_salary']=vacancy_salary
+#                 if data['currency'] == 'договорённости':
+#                     data['currency'] = None
+#                     data['salary_min'] = None
+#                     data['salary_max'] = None
+#                 elif salary_text.find('-') > -1:
+#                     data['salary_min'] = int(re.sub('\D', '', salary_text.split('-')[0]))
+#                     data['salary_max'] = int(re.sub('\D', '', salary_text.split('-')[-1]))
+#                 elif salary_text.find('—') > -1:
+#                     data['salary_min'] = int(re.sub('\D', '', salary_text.split('—')[0]))
+#                     data['salary_max'] = int(re.sub('\D', '', salary_text.split('—')[-1]))
+#                 elif salary_text.find('от') > -1:
+#                     data['salary_min'] = int(re.sub('\D', '', salary_text))
+#                     data['salary_max'] = None
+#                 elif salary_text.find('до') > -1:
+#                     data['salary_min'] = None
+#                     data['salary_max'] = int(re.sub('\D', '', salary_text))
+#                 else:
+#                     data['salary_min'] = int(re.sub('\D', '', salary_text))
+#                     data['salary_max'] = int(re.sub('\D', '', salary_text))
+#                 vacancies.append(data)
+#                 insert_uniq(data)
+#             except AttributeError:
+#                 continue
+#         next_link = soup.find('a', {'class': 'f-test-link-Dalshe'})
+#         if next_link is None:
+#             break
+#         addit_params = next_link['href']
+#
+# # if pr_reload_file == 'yes':
+# #     with open("result.csv", "w", encoding="utf-8") as my_file:
+# #         my_file.write("")
+# #
+# # with open("result.csv", "a", encoding="utf-8") as my_file:
+# #     for v in vacancies:
+# #         try:
+# #             my_file.write(f"{v['site']};{v['link']};{v['vacancy']};{v['salary_min']};{v['salary_max']};{v['currency']}\n")
+# #         except KeyError:
+# #             print(v)
+import datetime
+print (str(datetime.datetime.now())[0:11])
--- lession-4/1.py
+++ lession-4/1.py
@@ -2,6 +2,7 @@ import requests
 from lxml import html
 from pprint import pprint
 from pymongo import MongoClient
+import datetime
 
 client = MongoClient('127.0.0.1', 27017)
 db = client['news']
@@ -20,34 +21,42 @@ def insert_uniq(data):
     else:
         return False
 
-def get_news(url, xpath_gen, xpath_link, xpath_name, xpath_date_gen, xpath_date, xpath_source, pr_source):
+def get_news(url, xpath_gen, xpath_link, xpath_name, xpath_page_gen, xpath_date, xpath_source, pr_source, pr_current_page_dt, pr_current_page_src):
     response = requests.get(url, headers=header)
     dom = html.fromstring(response.text)
     items = dom.xpath(xpath_gen)
     news = []
+    date = None
+
     for item in items:
         new = {}
-        source = url
         link = item.xpath(xpath_link)
         name = item.xpath(xpath_name)
-        print(link)
-        new['link'] = url + link[0].replace(url, '')
+        source = None if xpath_source is None else item.xpath(xpath_source)
+
+        date = item.xpath(xpath_date)
+        new['link'] = url + link[0] if pr_source else link[0]
         new['name'] = name[0]
+        new['source'] = source[0] if pr_current_page_src and xpath_source is not None else url
+        new['date'] = date[0] if pr_current_page_dt else None
 
-        response_date = requests.get(new['link'], headers=header)
-        dom_date = html.fromstring(response_date.text)
-        items_date = dom_date.xpath(xpath_date_gen)
-        for item_date in items_date:
-            date = item_date.xpath(xpath_date)
-            if not pr_source:
-                source = item_date.xpath(xpath_source)
-        new['date'] = date[0]
-        new['source'] = source
+
+        if not pr_current_page_dt or not pr_current_page_src:
+            response_date = requests.get(new['link'], headers=header)
+            dom_date = html.fromstring(response_date.text)
+            items_date = dom_date.xpath(xpath_page_gen)
+
+            for item_date in items_date:
+                date = item_date.xpath(xpath_date)
+                source = None if xpath_source is None else item_date.xpath(xpath_source)
+                new['source'] = source[0] if not pr_current_page_src and xpath_source is not None else new['source']
+                new['date'] = date[0] if not pr_current_page_dt else new['date']
         news.append(new)
-        # insert_uniq(new)
-    # for new in newsdb.find():
-    #     pprint(new)
-    pprint(news)
+        insert_uniq(new)
 
-# get_news('https://lenta.ru/', "//div[@class='b-yellow-box__wrap']/div[@class='item']", ".//a/@href", ".//a/text()", "//div[@class='b-topic__info']/time[@class='g-date']", "./@datetime", None, True)
-get_news('https://news.mail.ru/', "//div[@name='clb20268335']//a[contains(@class,'photo_full')]", "//div[@name='clb20268335']//a[contains(@class,'photo_full')]//a/@href", ".//span[contains(@class,'photo__title')]/text()", "//div[contains(@class,'breadcrumbs')]", "./@datetime", "//div[contains(@class,'breadcrumbs')]//a/@href", True)
+
+get_news('https://lenta.ru/', "//div[@class='b-yellow-box__wrap']/div[@class='item']", ".//a/@href", ".//a/text()", "//div[@class='b-topic__info']/time[@class='g-date']", "./@datetime", None, True, False, None)
+get_news('https://news.mail.ru/', "//div[@name='clb20268335']//li[contains(@class,'list__item')] | //div[@name='clb20268335']//div[contains(@class,'daynews__item')]", "//a[contains(@class,'photo_full')]/@href | //a[@class='list__text']/@href", ".//span[contains(@class,'photo__title')]/text() | //a[@class='list__text']/text()", "//div[contains(@class,'breadcrumbs')]", "//@datetime", "//a[contains(@class,'breadcrumbs__link')]/@href", False, False, False)
+get_news('https://yandex.ru/news/', "//div[contains(@class,'mg-grid__col_sm_9')]/div[position()=5]//article", ".//a/@href", ".//a/h2/text()", "//div[@class='news-story__head']", ".//span[@class='mg-card-source__time']/text()", "./a/@href", False, True, False)
+for new in newsdb.find():
+    pprint(new)
