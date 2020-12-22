bf1823cc397d6d23cbaccea5c25572a33f2d6fbc
kt-tm <katya.melnikowa@mail.ru>
2020-12-23 02:03:25 +0300

    ДЗ 5

--- lession-5/2.py
+++ lession-5/2.py
@@ -1,13 +1,24 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
 from pprint import pprint
+from pymongo import MongoClient
 import time
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
 import json
 
+client = MongoClient('127.0.0.1', 27017)
+db = client['mvideo']
+mvideodb = db.mvideo
 
+def insert_uniq(data):
+    if mvideodb.count_documents({'productId': data['productId']}) == 0:
+        mvideodb.insert_one({
+            "productPriceLocal": data['productPriceLocal'],
+            "productId": data['productId'],
+            "productName": data['productName'],
+            "href": data['href']})
+        return True
+    else:
+        return False
 
 chrome_options = Options()
 chrome_options.add_argument('start-maximized')
@@ -15,30 +26,25 @@ driver = webdriver.Chrome(executable_path='./chromedriver.exe', options=chrome_o
 
 result = []
 driver.get('https://www.mvideo.ru/')
-# elem = driver.find_element_by_xpath("//div[@class='section']//div[contains(@class,'gallery-content accessories-new ')]")
-# print(elem.text)
-cnt = 0
 count = int(json.loads(driver.find_element_by_xpath("//div[@class='section']//div[contains(@class,'gallery-content accessories-new ')]//ul[contains(@class,'accessories-product-list')]").get_attribute('data-init-param'))['ajaxContentLoad']['total'])
-# print(count)
-while count >= 0:
-    count-=1
-    elements = driver.find_elements_by_xpath("//div[@class='section']//div[contains(@class,'gallery-content accessories-new ')]//ul[contains(@class,'accessories-product-list')]//li//a")
+while True:
+    time.sleep(3)
+    elements = driver.find_elements_by_xpath("//div[contains(text(),'Хиты продаж')]/ancestor::div[@class='section']//div[contains(@class,'gallery-content accessories-new ')]//ul[contains(@class,'accessories-product-list')]/li/div/div/div/a")
     for elem in elements:
         data = {}
-        print(elem.text)
-        data = elem.get_attribute("data-product-info")
-        print(data)
-        # data = elem.get_attribute("data-product-info")
-        result.append(data)
-        cnt += 1
-        print(f"cnt={cnt}")
-    print(f"cnt2={cnt}")
-    # print(result)
-    # print(count)
-    button = driver.find_element_by_class_name("//div[@class='section']//div[contains(@class,'gallery-content accessories-new ')]//a[contains(@class, 'next-btn')]")
+        data = json.loads(elem.get_attribute("data-product-info"))
+        data['href'] = elem.get_attribute("href")
+        insert_uniq(data)
+        result.append(data['href'])
+    result = list(set(result))
+    if count == len(result):
+        break
+    button = driver.find_element_by_xpath("//div[contains(text(),'Хиты продаж')]/ancestor::div[@class='section']//div[contains(@class,'gallery-content accessories-new ')]//a[contains(@class, 'next-btn')]")
     button.click()
-
-# pprint(result)
-# //div[@class='u-mb-12 u-mt-24 gallery-layout__title-block']
-# //ul[contains(@class,'accessories-product-list')]
 driver.close()
+
+cntm = 0
+for good in mvideodb.find():
+    pprint(good)
+    cntm+=1
+print(f"cntm={cntm}")
